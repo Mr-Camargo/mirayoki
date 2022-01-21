eb70aae8fa39148ecbc457f2a05d060a70c24ffb
Sebastián Camargo <camargo@tutamail.com>
2022-01-20 22:05:27 -0500

    Improved format for quote dates
    As many countries use the DD-MM-YYYY format and other countries the MM-DD-YYYY one, Mirayoki will know place the date on a quote in a format that will be understood by everyone. Using "Month Day, Year"
    
    Example: "January 20, 2022"

--- commands/quote.js
+++ commands/quote.js
@@ -2,7 +2,7 @@ module.exports = {
     name: 'quote',
     aliases: ['q'],
     description: "Record a string of text on the #quotes channel, usually something a member said that you want to remember",
-    execute(message, args, cmd, client, Discord, profileData) {
+    async execute(message, args, cmd, client, Discord, profileData) {
 
         let quote = args.join(' ');
         /* This will merge all the words the user has typed on the message,
@@ -15,10 +15,62 @@ module.exports = {
         so it can then be displayed on the quote embed*/
 
         function calculateMonth() {
-           let fakeMonth = date.getMonth();
-           let realMonth = fakeMonth + 1;
-           return realMonth;
+            let realMonth;
+            let monthLanguage = profileData.language;
+            switch (date.getMonth()) {
+                case 0:
+                    realMonth = "January";
+                    if (monthLanguage === "es") realMonth = "Enero"
+                    break;
+                case 1:
+                    realMonth = "February";
+                    if (monthLanguage === "es") realMonth = "Febrero"
+                    break;
+                case 2:
+                    realMonth = "March";
+                    if (monthLanguage === "es") realMonth = "Marzo"
+                    break;
+                case 3:
+                    realMonth = "April";
+                    if (monthLanguage === "es") realMonth = "Abril"
+                    break;
+                case 4:
+                    realMonth = "May";
+                    if (monthLanguage === "es") realMonth = "Mayo"
+                    break;
+                case 5:
+                    realMonth = "June";
+                    if (monthLanguage === "es") realMonth = "Junio"
+                    break;
+                case 6:
+                    realMonth = "July";
+                    if (monthLanguage === "es") realMonth = "Julio"
+                    break;
+                case 7:
+                    realMonth = "August";
+                    if (monthLanguage === "es") realMonth = "Agosto"
+                    break;
+                case 8:
+                    realMonth = "September";
+                    if (monthLanguage === "es") realMonth = "Septiembre"
+                    break;
+                case 9:
+                    realMonth = "October";
+                    if (monthLanguage === "es") realMonth = "Octubre"
+                    break;
+                case 10:
+                    realMonth = "November";
+                    if (monthLanguage === "es") realMonth = "Noviembre"
+                    break;
+                case 11:
+                    realMonth = "December";
+                    if (monthLanguage === "es") realMonth = "Diciembre"
+                    break;
+            };
+            return realMonth;
         };
+        /* This function adds a number to the month array because arrays start at 0,
+        so it would be werid for January to be Month 0, and December to be Month 11 */
 
         const noChannelEN = new Discord.MessageEmbed()
 
@@ -40,21 +92,21 @@ module.exports = {
             .setColor('#FFC300')
             .setAuthor(message.author.tag, message.author.displayAvatarURL({ dynamic: true }))
             .setDescription(`*${quote}*`)
-            .setFooter(`${day}/${month}/${year}`)
+            .setFooter(`${month} ${day}, ${year}`);
 
         const sentQuoteEN = new Discord.MessageEmbed()
             .setColor('#55C2FF')
             .setAuthor(message.author.tag, message.author.displayAvatarURL({ dynamic: true }))
             .setTitle('Quote sent')
             .setDescription(`You just said *"` + quote + `"*, and will be preserved, for life, in the #quotes channel.`)
-            .setFooter(`${day}/${month}/${year}`);
+            .setFooter(`Sent on ${month} ${day}, ${year}`);
 
         const sentQuoteES = new Discord.MessageEmbed()
             .setColor('#55C2FF')
             .setAuthor(message.author.tag, message.author.displayAvatarURL({ dynamic: true }))
             .setTitle('Quote enviada')
             .setDescription(`Acabas de decir *"` + quote + `"*, y será preservado, de por vida, en el canal de #quotes.`)
-            .setFooter(`${day}/${month}/${year}`);
+            .setFooter(`Enviado en ${month} ${day}, ${year}`);
 
         const noQuoteEN = new Discord.MessageEmbed()
             .setColor('#FF5733')
