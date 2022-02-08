d845b7a55c583cc7c8c33956de0e0fcdec47a41a
Sebastián Camargo <camargo@tutamail.com>
2022-02-08 14:14:30 -0500

    Quick fix regarding new eslint implementation
    I should be careful to push all changes in here so eslint doesn't run into code that wasn't updated on the remote.

--- src/commands/Information/color.js
+++ src/commands/Information/color.js
@@ -1,54 +1,54 @@
 const { SlashCommandBuilder } = require('@discordjs/builders');
-const { MessageEmbed, MessageActionRow, MessageSelectMenu } = require('discord.js');
+const { MessageActionRow, MessageSelectMenu } = require('discord.js');
 
 module.exports = {
-    data: new SlashCommandBuilder()
-        .setName('color')
-        .setDescription(`Choose your favorite color`),
+	data: new SlashCommandBuilder()
+		.setName('color')
+		.setDescription('Choose your favorite color'),
         
-    async execute(interaction, client) {
-        const row = new MessageActionRow()
-        .addComponents(
-            new MessageSelectMenu()
-            .setCustomId('color-select')
-            .setPlaceholder('Pick a color...')
-            .setMinValues(1)
-            .setMaxValues(2)
-            .addOptions([
-                {
-                    label: 'Red',
-                    description: 'Your color is red',
-                    value: 'red'
-                },
-                {
-                    label: 'Blue',
-                    description: 'Your color is blue',
-                    value: 'blue'
-                },
-                {
-                    label: 'Yellow',
-                    description: 'Your color is yellow',
-                    value: 'yellow'
-                },
-                {
-                    label: 'Green',
-                    description: 'Your color is green',
-                    value: 'green'
-                },
-                {
-                    label: 'Purple',
-                    description: 'Your color is purple',
-                    value: 'purple'
-                },
-                {
-                    label: 'Orange',
-                    description: 'Your color is orange',
-                    value: 'orange'
-                }
+	async execute(interaction, client) {
+		const row = new MessageActionRow()
+			.addComponents(
+				new MessageSelectMenu()
+					.setCustomId('color-select')
+					.setPlaceholder('Pick a color...')
+					.setMinValues(1)
+					.setMaxValues(2)
+					.addOptions([
+						{
+							label: 'Red',
+							description: 'Your color is red',
+							value: 'red'
+						},
+						{
+							label: 'Blue',
+							description: 'Your color is blue',
+							value: 'blue'
+						},
+						{
+							label: 'Yellow',
+							description: 'Your color is yellow',
+							value: 'yellow'
+						},
+						{
+							label: 'Green',
+							description: 'Your color is green',
+							value: 'green'
+						},
+						{
+							label: 'Purple',
+							description: 'Your color is purple',
+							value: 'purple'
+						},
+						{
+							label: 'Orange',
+							description: 'Your color is orange',
+							value: 'orange'
+						}
 
-            ])
-        );
+					])
+			);
 
-        await interaction.reply({ content: 'What is your fav color?', components: [row]});
-    }
+		await interaction.reply({ content: 'What is your fav color?', components: [row]});
+	}
 };
